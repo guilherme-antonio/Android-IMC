b320859ebf90537f3792a15f5d900d78dc4d5bdc
Ruan7 <RJlana43@hotmail.com>
2019-03-08 20:25:30 -0300

    Initial commit

---
+++ app/src/main/java/guilherme/br/edu/ifsc/exercicioimc/MainActivity.java
@@ -0,0 +1,13 @@
+package guilherme.br.edu.ifsc.exercicioimc;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
