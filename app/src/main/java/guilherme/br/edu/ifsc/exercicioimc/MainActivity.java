a8642fa93847c934b0890b13e76175f796302ea4
Ruan7 <RJlana43@hotmail.com>
2019-03-08 22:08:48 -0300

    View CalculoIMC

--- app/src/main/java/guilherme/br/edu/ifsc/exercicioimc/MainActivity.java
+++ app/src/main/java/guilherme/br/edu/ifsc/exercicioimc/MainActivity.java
@@ -2,6 +2,9 @@ package guilherme.br.edu.ifsc.exercicioimc;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void calcularIMC(View view)
+    {
+        EditText alturaText = findViewById(R.id.altura);
+        EditText pesoText = findViewById(R.id.peso);
+
+        float altura = Float.valueOf(alturaText.getText().toString());
+        float peso = Float.valueOf(pesoText.getText().toString());
+
+        float imc = peso / (altura * altura);
+
+        TextView imcText = findViewById(R.id.imc);
+
+        imcText.setText(String.format("IMC: %.2f", imc));
     }
 }
