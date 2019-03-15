2f3e4896425d97a89268c61e6cc273e1d710aff1
Guilherme <guioliveira99@gmail.com>
2019-03-15 17:20:47 -0300

    Adicionando imagens

--- app/src/main/java/guilherme/br/edu/ifsc/exercicioimc/MainActivity.java
+++ app/src/main/java/guilherme/br/edu/ifsc/exercicioimc/MainActivity.java
@@ -1,9 +1,11 @@
 package guilherme.br.edu.ifsc.exercicioimc;
 
+import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -27,5 +29,36 @@ public class MainActivity extends AppCompatActivity {
         TextView imcText = findViewById(R.id.imc);
 
         imcText.setText(String.format("IMC: %.2f", imc));
+
+        ImageView imagem = findViewById(R.id.imagem);
+
+        Drawable imageDrawable;
+
+        if (imc < 18.5F)
+        {
+            imageDrawable = getDrawable(R.drawable.abaixopeso);
+        }
+        else if (imc <= 24.9F)
+        {
+            imageDrawable = getDrawable(R.drawable.normal);
+        }
+        else if (imc <= 29.9F)
+        {
+            imageDrawable = getDrawable(R.drawable.sobrepeso);
+        }
+        else if (imc <= 34.9F)
+        {
+            imageDrawable = getDrawable(R.drawable.obesidade1);
+        }
+        else if (imc <= 39.9F)
+        {
+            imageDrawable = getDrawable(R.drawable.obesidade2);
+        }
+        else
+        {
+            imageDrawable = getDrawable(R.drawable.obesidade3);
+        }
+
+        imagem.setImageDrawable(imageDrawable);
     }
 }
